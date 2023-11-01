13a5c218067a892dad4d1c3fbab2fb2d1abe6ba9
Cheh91 <4eh91@ukr.net>
2023-11-01 21:57:43 +0200

    checkbox

--- app/js/main.js
+++ app/js/main.js
@@ -19,12 +19,19 @@ window.addEventListener("DOMContentLoaded", () => {
 
     form.addEventListener("submit", (e) => {
         e.preventDefault();
-        console.log(e);
+        // console.log(e);
 
-        let newItem = formValue.value;
+        let newTodo = {
+            todo: formValue.value,
+            checked: false,
+        }
 
-        if (newItem.trim().length > 0) {
-            listArray.technology.push(newItem);
+        // let newItem = formValue.value;
+
+        if (newTodo.todo.trim().length > 0) {
+            listArray.technology.push(newTodo);
+
+            console.log(listArray.technology);
             // listArray.technology.sort();
             listAdd(list, listArray.technology);
             listSort();
@@ -35,10 +42,6 @@ window.addEventListener("DOMContentLoaded", () => {
         formValue.value = "";
     });
 
-    counter.innerHTML = `
-        <div class="counter__item">${listArray.technology.length} <span>items</span></div>
-    `;
-
 
     const listAdd = (listParent, listTechnology) => {
         listParent.innerHTML = "";
@@ -46,9 +49,13 @@ window.addEventListener("DOMContentLoaded", () => {
         listTechnology.forEach((item, i) => {
 
             listParent.innerHTML += `
-                <li class="list__item"><input type="checkbox"><span class="list__item-text">${i + 1}. ${item}</span><span class="list__item-del"></span></li>
+                <li class="list__item">
+                    <input type="checkbox" id="item_${i}" ${item.checked ? "checked" : ""}>
+                    <label class="list__item-text" for="item_${i}">${item.todo}</label><span class="list__item-del"></span>
+                </li>
             `;
             count();
+            // localSave();
 
             const btnDel = document.querySelectorAll(".list__item-del");
 
@@ -65,8 +72,26 @@ window.addEventListener("DOMContentLoaded", () => {
             });
 
         });
+        localSave();
     };
     listAdd(list, listArray.technology);
+
+    list.addEventListener("change", function(e){
+        // console.log(e.target.getAttribute("id"));
+        let idInput = e.target.getAttribute("id");
+        let forLabel = list.querySelector('[for='+ idInput +']');
+        let valueLabel = forLabel.innerHTML;
+        console.log(valueLabel);
+
+        listArray.technology.forEach((item) => {
+            // console.log(item.todo);
+            if(item.todo == valueLabel){
+                item.checked = !item.checked;
+                localSave();
+            }
+        })
+    });
+    
 
 
     function count() {
@@ -88,17 +113,132 @@ window.addEventListener("DOMContentLoaded", () => {
 
     function listSort() {
         sortItem.addEventListener("click", () => {
-            listArray.technology.sort();
+            listArray.technology.sort((a, b) => a.todo > b.todo ? 1 : -1);
             listAdd(list, listArray.technology);
-            localSave()
+            localSave();
         });
-        
     }
     listSort();
 
     function localSave(){
         localStorage.setItem("items", JSON.stringify(listArray.technology));
     }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    // const listArray = {
+    //     // technology: [
+    //     //     // "html",
+    //     //     // "css",
+    //     //     // "js"
+    //     // ]
+    //     technology: JSON.parse(localStorage.getItem("items"))
+    // };
+
+    // const form = document.querySelector(".form-item"),
+    //     formValue = document.querySelector(".form__value"),
+    //     list = document.querySelector(".list"),
+    //     counter = document.querySelector(".counter"),
+    //     removeAll = document.querySelector(".remove-all"),
+    //     sortItem = document.querySelector(".sort-all");
+
+
+    // form.addEventListener("submit", (e) => {
+    //     e.preventDefault();
+    //     console.log(e);
+
+    //     let newItem = formValue.value;
+
+    //     if (newItem.trim().length > 0) {
+    //         listArray.technology.push(newItem);
+    //         // listArray.technology.sort();
+    //         listAdd(list, listArray.technology);
+    //         listSort();
+    //         localSave();
+    //     }
+
+    //     // form.reset();
+    //     formValue.value = "";
+    // });
+
+    // counter.innerHTML = `
+    //     <div class="counter__item">${listArray.technology.length} <span>items</span></div>
+    // `;
+
+
+    // const listAdd = (listParent, listTechnology) => {
+    //     listParent.innerHTML = "";
+
+    //     listTechnology.forEach((item, i) => {
+
+    //         listParent.innerHTML += `
+    //             <li class="list__item"><input type="checkbox"><span class="list__item-text">${i + 1}. ${item}</span><span class="list__item-del"></span></li>
+    //         `;
+    //         count();
+
+    //         const btnDel = document.querySelectorAll(".list__item-del");
+
+    //         btnDel.forEach((item, x) => {
+    //             item.addEventListener("click", () => {
+    //                 item.parentElement.remove();
+    //                 listTechnology.splice(x, 1);
+    //                 listAdd(list, listArray.technology);
+    //                 // if (listArray.technology.length == 0) {
+    //                     count();
+    //                 // }
+    //                 localSave();
+    //             });
+    //         });
+
+    //     });
+    // };
+    // listAdd(list, listArray.technology);
+
+
+    // function count() {
+    //     counter.innerHTML = `
+    //         <div class="counter__item">${listArray.technology.length} <span>items</span></div>
+    //     `;
+    // }
+    // count();
+
+    // function listRemove() {
+    //     removeAll.addEventListener("click", () => {
+    //         list.innerHTML = "";
+    //         listArray.technology.splice(0);
+    //         count();
+    //         localSave();
+    //     });
+    // }
+    // listRemove();
+
+    // function listSort() {
+    //     sortItem.addEventListener("click", () => {
+    //         listArray.technology.sort();
+    //         listAdd(list, listArray.technology);
+    //         localSave()
+    //     });
+    // }
+    // listSort();
+
+    // function localSave(){
+    //     localStorage.setItem("items", JSON.stringify(listArray.technology));
+    // }
 
 
 
